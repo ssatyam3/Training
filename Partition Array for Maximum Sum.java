53658b32e593c906dabcc19c8923c70b93197f35
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-07-30 11:26:47 +0530

    Create Partition Array for Maximum Sum.java

---
+++ Partition Array for Maximum Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSumAfterPartitioning(int[] A, int K) {
+        int len = A.length;
+        int[] dp = new int[len];
+        
+        int max = A[0];
+        for(int i = 0; i < K; i++){
+            max = Math.max(max, A[i]);
+            dp[i] = max * (i+1);
+        }
+        
+        for(int i = K;i < len; i++){
+            max = A[i];
+            for(int j = 1; j <= K; j++){
+                max = Math.max(max, A[i - j + 1]);
+                dp[i] = Math.max(dp[i], dp[i - j] + max * j);
+            }
+        }
+        
+        return dp[len - 1];
+    }
+}
