7f04d282e2d26d8726360a1a21c38e79a011a86d
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-08-06 15:29:21 +0530

    Create bfs.java

---
+++ bfs.java
@@ -0,0 +1,36 @@
+ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); // adjacency list representation
+
+int n; // number of nodes
+int s; // source vertex
+
+
+LinkedList<Integer> q = new LinkedList<Integer>();
+boolean used[] = new boolean[n];
+int d[] = new int[n];
+int p[] = new int[n];
+
+q.push(s);
+used[s] = true;
+p[s] = -1;
+while (!q.isEmpty()) {
+    int v = q.pop();
+    for (int u : adj.get(v)) {
+        if (!used[u]) {
+            used[u] = true;
+            q.push(u);
+            d[u] = d[v] + 1;
+            p[u] = v;
+        }
+    }
+}
+
+if (!used[u]) {
+    System.out.println("No path!");
+} else {
+    ArrayList<Integer> path = new ArrayList<Integer>();
+    for (int v = u; v != -1; v = p[v])
+        path.add(v);
+    Collections.reverse(path);
+    for(int v : path)
+        System.out.println(v);
+}
